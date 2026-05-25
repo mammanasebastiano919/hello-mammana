9ebe216e7a1e2b48287139102d293aac91c63234
mammanasebastiano919 <mammanasebastiano919@gmail.com>
2026-05-25 11:54:04 +0000

    Aggiunta stampa da VECTOR

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,16 @@
 import java.util.*;
+import java.util.Vector;
 
 public class helloworld{
     static void main(String[] args){
         System.out.println("Hello Seba!");
 
         //Array di caratteri con lettere del nome "Seba"
-        char[] lettere = {'S', 'e', 'b', 'a'};
+       Vector<Character> lettere = new Vector<>();
+       lettere.add('S');
+       lettere.add('e');
+       lettere.add('b');
+       lettere.add('a');
 
         //Stampa le lettere dell'array
         for(char lettera : lettere){
