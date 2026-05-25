549ce5db2186dfbda8e67ac86fdd43f1bdb74236
mammanasebastiano919 <mammanasebastiano919@gmail.com>
2026-05-25 11:09:12 +0000

    Stampa nome 3 volte

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class helloworld{
     static void main(String[] args){
         System.out.println("Hello Sebastiano!");
+        System.out.println("Hello Sebastiano!");
+        System.out.println("Hello Sebastiano!");
     }
 }
