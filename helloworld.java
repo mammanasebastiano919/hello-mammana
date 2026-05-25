97d086b6d35c48ed87471deebaccb3b05a6835a1
mammanasebastiano919 <mammanasebastiano919@gmail.com>
2026-05-25 11:45:38 +0000

    Aggiunta stampa da ARRAY STATICO

--- helloworld.java
+++ helloworld.java
@@ -2,8 +2,14 @@ import java.util.*;
 
 public class helloworld{
     static void main(String[] args){
-        System.out.println("Hello Sebastiano!");
-        System.out.println("Hello Sebastiano!");
-        System.out.println("Hello Sebastiano!");
+        System.out.println("Hello Seba!");
+
+        //Array di caratteri con lettere del nome "Seba"
+        char[] lettere = {'S', 'e', 'b', 'a'};
+
+        //Stampa le lettere dell'array
+        for(char lettera : lettere){
+            System.out.println(lettera + " ");
+        }
     }
 }
